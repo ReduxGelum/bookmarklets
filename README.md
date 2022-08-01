d97078a5109966f669c1b8c17d29626c01c80eb7
reduxGelum <reduxgelum@gmail.com>
2022-08-01 10:35:04 +0300

    updated default rates, cleanup

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Youtube speed bookmarklet
+# Video playback speed hotkeys bookmarklet
+## Works with any website that uses `video` tag e.g. youtube, tiktok
 
-1. add as a bookmark in browser `javascript:document.onkeydown = function (e) { r={q:1,a:1.5,w:2,s:3,e:15}; k=e.key.toLowerCase(); if (k in r) { document.getElementsByTagName('video')[0].playbackRate=r[k]; } };undefined`
+1. add as a bookmark in browser `javascript:(function(s){document.onkeydown=function(e){r={q:1,a:1.75,w:2.5,s:5,e:15};k=e.key.toLowerCase();if(k in r){document.querySelector('video').playbackRate=r[k]}}})();`
 1. click when youtube is open
 1. press hotkeys to speed up
   * `q` - normal speed
-  * `a` - 1.5x speed
-  * `w` - 2x speed
-  * `s` - 3x speed
+  * `a` - 1.75x speed
+  * `w` - 2.5x speed
+  * `s` - 5x speed
   * `e` - 15x speed
--- bookmarklets.js
+++ bookmarklets.js
@@ -1,22 +1,12 @@
-// convert to inline bookmarklet
-// javascript:{javascript code};undefined
-
-// keys for youtube.com playback rates 1 2 4, keys n b v
-document.onkeydown = function (e) {
-    e.which == 78
-        ? document.getElementsByTagName('video')[0].playbackRate=1
-        : e.which == 66
-            ? document.getElementsByTagName('video')[0].playbackRate=2
-            : e.which == 86
-                ? document.getElementsByTagName('video')[0].playbackRate=4
-                : null
-}
-
-// keys for youtube.com playback rates 1 1.5 2 4, keys q a w e
-document.onkeydown = function (e) {
-    k = e.key.toLowerCase();
-    r = {q:1, a:1.5, w:2, e:4};
-    if (k in r) {
-        document.getElementsByTagName('video')[0].playbackRate = r[k];
+javascript:(function(s){
+    document.onkeydown = function (e) {
+        r={q:1,a:1.75,w:2.5,s:5,e:15};
+        k=e.key.toLowerCase();
+        if (k in r) {
+            document.querySelector('video').playbackRate=r[k]
+        }
     }
-}
+})();
+
+// known issues
+//  * typing in text input activates speedchange
